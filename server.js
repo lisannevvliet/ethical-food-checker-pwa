e7a773e360776a951a1c2bcb7e43237af89de1e0
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-03-21 15:12:41 +0100

    node-fetch geïmplementeerd

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-// Load Express.
-var express = require('express')
+// Load Node.js modules.
+import express from 'express'
+import fetch from 'node-fetch'
+
+// Get the products from the API.
+const response = await fetch('https://world.openfoodfacts.org/cgi/search.pl?search_terms=melk&search_simple=1&action=process&json=1&page=1')
+const data = await response.json()
 
 // Initialise Express.
 var app = express()
@@ -16,5 +21,5 @@ app.listen(8080)
 // *** GET Routes - display pages. ***
 // Root Route.
 app.get('/', function (_req, res) {
-    res.render('index')
+    res.render('index', { data: data.products })
 })
