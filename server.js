818b21b0643cae5c27497a8b363fee83fb0c77d7
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-03-28 11:16:04 +0200

    Opgeslagen in Boolean of de zoekopdracht een barcode is

--- server.js
+++ server.js
@@ -21,9 +21,11 @@ app.get('/', async function (_req, res) {
 })
 
 app.get('/search', async function (req, res) {
+    // Boolean which shows if the search query is a barcode.
+    const barcode = /^\d+$/.test(req.query.q)
+
     // Save the correct URL based on if the search query is a barcode or not.
-    let url = /^\d+$/.test(req.query.q)
-    ? `https://world.openfoodfacts.org/cgi/search.pl?code=${req.query.q}&search_simple=1&action=process&json=1&page=1`
+    const url = barcode ? `https://world.openfoodfacts.org/cgi/search.pl?code=${req.query.q}&search_simple=1&action=process&json=1&page=1`
     : `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${req.query.q}&search_simple=1&action=process&json=1&page=1`
 
     // Get the products from the API.
@@ -31,8 +33,8 @@ app.get('/search', async function (req, res) {
     const data = await response.json()
 
     if (data.products == 0) {
-        // Find out if the search query is a product name or barcode.
-        let type = /^\d+$/.test(req.query.q) ? 'barcode' : 'name'
+        // Assign a name to the search query type.
+        const type = barcode ? 'barcode' : 'name'
         res.render('error', { query: req.query.q, type: type })
     } else {
         res.render('results', { query: req.query.q, products: data.products })
