5587574c48c477b9afa7965a370b105c38476889
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-03-28 11:37:26 +0200

    "Explanation" gerepareerd

---
+++ public/scripts/script.js
@@ -0,0 +1,14 @@
+function $(element) {
+    // Return the Element object of the corresponding element.
+    return document.querySelector(element)
+}
+
+// If the explanation is clicked, show the section.
+$("#explanation").addEventListener("click", function() {
+    $("section").classList.add("block")
+})
+
+// If the explanation is closed, hide the section.
+$("section button").addEventListener("click", function() {
+    $("section").classList.remove("block")
+})
