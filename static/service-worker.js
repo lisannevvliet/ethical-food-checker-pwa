acbfd8895bd99a8f01eb72e1df357f56417560ec
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-04-04 16:24:08 +0200

    Comments toegevoegd aan service worker

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,4 +1,4 @@
-// / cashes index.ejs as HTML.
+// Cache all static files, as well as the index.ejs and offline.ejs file.
 const CORE_FILES = [
     "/fonts/Lato-Bold.ttf",
     "/fonts/Lato-Regular.ttf",
@@ -12,6 +12,7 @@ const CORE_FILES = [
     "/offline"
 ]
 
+// Install the service worker.
 self.addEventListener("install", function(event) {
     event.waitUntil(
         caches.open("core-cache").then(function(cache) {
@@ -20,40 +21,40 @@ self.addEventListener("install", function(event) {
             return cache.addAll(CORE_FILES)
         })
     )
-    console.log("Service worker installed.")
 })
 
-self.addEventListener("activate", function(_event) {
-    console.log("Service worker activated.")
-})
+// Activate the service worker.
+self.addEventListener("activate", function(_event) { })
 
+// Fetch the service worker.
 self.addEventListener("fetch", function(event) {
     const url = new URL(event.request.url)
-    // Check if any of the requested files already exists in the core-cache.
+    // Check if any of the requested files already exists in the core-cache and serve them.
     if (event.request.method === "GET" && CORE_FILES.includes(url.pathname)) {
         event.respondWith(
             caches.open("core-cache")
                 .then(cache => cache.match(event.request.url))
         )
-    // Only cache the HTML file.
+    // Only request the HTML file, all the other files are already in the cache.
     } else if (event.request.method === "GET" && (event.request.headers.get("accept") !== null && event.request.headers.get("accept").includes("text/html"))) {
         event.respondWith(
             // Stale-while-revalidate (see https://web.dev/offline-cookbook/#stale-while-revalidate).
             caches.open("dynamic-cache").then(function(cache) {
                 return cache.match(event.request)
+                    // Retrieve, cache and serve the HTML file.
                     .then(function(response) {
                         var fetchPromise = fetch(event.request).then(function(networkResponse) {
                             cache.put(event.request, networkResponse.clone())
                             return networkResponse
                         })
-                    return response || fetchPromise
+                        return response || fetchPromise
                     })
+                    // Serve the offline page if the HTML is not in the cache and there is no internet.
                     .catch(_error => {
                         return caches.open("core-cache")
                             .then(cache => cache.match("/offline"))
                     })
-            }),
-          )
+            })
+        )
     }
-    console.log("Service worker fetched.")
 })
